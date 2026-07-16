d2bd4a365497dd7c75af19752f13ba5bebb4f40b
zenith-devv <maciejektomaszek10@gmail.com>
2026-07-16 22:25:53 +0200

    Added message to add gitman binary location to PATH in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@
 mkdir -p ~/.local/share/gitman/repos
 nimble install --depsOnly -y
 nim c -d:release -d:ssl -o:"$HOME/.local/bin/gitman" src/main.nim
+echo "Finished installing gitman. Make sure to add ~/.local/bin to your PATH."
